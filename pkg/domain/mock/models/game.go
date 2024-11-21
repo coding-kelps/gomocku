fdbdb44ec05a503f6ff99f97796b671402f38725
Guilhem Santé <guilhem.snt@gmail.com>
2024-11-22 00:08:38 +0100

    feat: add board handler implementation

--- pkg/domain/mock/models/game.go
+++ pkg/domain/mock/models/game.go
@@ -10,6 +10,18 @@ type Board struct {
 	size uint8
 }
 
+type Player uint8
+
+const (
+	White Player = 0
+	Black Player = 1
+)
+
+type Turn struct {
+	Position Position
+	Player Player
+}
+
 func NewBoard(size uint8) *Board {
 	cells := make([][]uint8, size)
 	for i := range cells {
--- pkg/inbound/stdio/parsers.go
+++ pkg/inbound/stdio/parsers.go
@@ -67,3 +67,38 @@ func (std *Stdio) parsePosition(s string) (models.Position, error) {
 
 	return p, nil
 }
+
+type WrongFieldError struct {
+}
+
+func (e *WrongFieldError) Error() string {
+	return "playing field can only be either 1 (Own) or 2 (Opponent)"
+}
+
+func (std *Stdio) parseBoardTurn(s string) (models.Turn, error) {
+	r := regexp.MustCompile(`^(\d+,\d+),(\d)$`)
+	m := r.FindStringSubmatch(s)
+	if len(m) != 3 {
+		return models.Turn{}, &InvalidFormatError{s, r}
+	}
+
+	position, err := std.parsePosition(m[1])
+	if err != nil {
+		return models.Turn{}, err
+	}
+
+	nb, err := strconv.ParseUint(m[2], 0, 8)
+	if err != nil {
+		return models.Turn{}, err
+	}
+
+	player := models.Player(nb - 1)
+	if !(player == models.White || player == models.Black) {
+		return models.Turn{}, &WrongFieldError{}
+	}
+
+	return models.Turn{
+		Position: position,
+		Player: player,
+	}, nil
+}
