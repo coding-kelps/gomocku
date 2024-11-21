c63bd50b44c9905e35eb302756c26c634c72af59
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2024-11-22 00:48:21 +0100

    feat: add board state management

--- pkg/domain/mock/service.go
+++ pkg/domain/mock/service.go
@@ -1,8 +1,8 @@
 package mock
 
 import (
-	"github.com/coding-kelps/gomocku/pkg/domain/mock/ports"
 	"github.com/coding-kelps/gomocku/pkg/domain/mock/models"
+	"github.com/coding-kelps/gomocku/pkg/domain/mock/ports"
 )
 
 type Mock struct {
@@ -16,10 +16,10 @@ func NewMock() ports.Mock {
 	return &Mock{
 		board: nil,
 		about: map[string]string{
-			"name": "gomocku",
+			"name":    "gomocku",
 			"version": "0.1",
-			"author": "Coding Kelps",
-			"desc": "A mock AI for testing",
+			"author":  "Coding Kelps",
+			"desc":    "A mock AI for manager testing",
 		},
 	}
 }
@@ -31,7 +31,14 @@ func (m *Mock) RespondStart(size uint8) error {
 }
 
 func (m *Mock) RespondTurn(p models.Position) (models.Position, error) {
-	// Set corresponding cell in board
+	if m.board == nil {
+		return models.Position{}, &models.BoardUnsetError{}
+	}
+
+	err := m.board.SetCell(p, models.OpponentStone)
+	if err != nil {
+		return models.Position{}, err
+	}
 
 	// Arbitrary Choose cell in board as move
 	move := models.Position{X: 0, Y: 0}
@@ -40,14 +47,42 @@ func (m *Mock) RespondTurn(p models.Position) (models.Position, error) {
 }
 
 func (m *Mock) RespondBegin() (models.Position, error) {
+	if m.board == nil {
+		return models.Position{}, &models.BoardUnsetError{}
+	}
+
 	// Arbitrary Choose cell in board as move
 	move := models.Position{X: 0, Y: 0}
 
 	return move, nil
 }
 
-func (m *Mock) RespondBoard(p []models.Turn) (models.Position, error) {
+func playerToCellStatus(p models.Player) (models.CellStatus, error) {
+	if p == models.Us {
+		return models.OwnStone, nil
+	} else if p == models.Opponent {
+		return models.OpponentStone, nil
+	} else {
+		return 0, &models.InvalidPlayerError{PlayerValue: p}
+	}
+}
+
+func (m *Mock) RespondBoard(turns []models.Turn) (models.Position, error) {
+	if m.board == nil {
+		return models.Position{}, &models.BoardUnsetError{}
+	}
 	// Set corresponding cells in board
+	for _, t := range turns {
+		status, err := playerToCellStatus(t.Player)
+		if err != nil {
+			return models.Position{}, err
+		}
+
+		err = m.board.SetCell(t.Position, status)
+		if err != nil {
+			return models.Position{}, err
+		}
+	}
 
 	// Arbitrary Choose cell in board as move
 	move := models.Position{X: 0, Y: 0}
@@ -60,6 +95,10 @@ func (m *Mock) RespondInfo() error {
 }
 
 func (m *Mock) RespondEnd() error {
+	if m.board == nil {
+		return &models.BoardUnsetError{}
+	}
+
 	return nil
 }
 
